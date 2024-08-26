5867766d279e6222e44499f8616667b3704e57a9
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-08-26 11:34:24 +0200

    Add description to readme.md

--- src/components/ProjectCardComponent.vue
+++ src/components/ProjectCardComponent.vue
@@ -14,7 +14,7 @@ export default {
 
     <li class="card col-3">
       <img :src="item.image ? item.image : 'not-avaible.jpg'" class="card-img-top position-relative" height="200" alt="...">
-      <img v-if="item.status === 'work in progress'" src="/public/work-in-progress.jpg" class="position-absolute end-0" width="100" alt="...">
+      <img v-if="item.status === 'work in progress'" src="/work-in-progress.jpg" class="position-absolute end-0" width="100" alt="...">
       <div class="card-body">
         <h5 class="card-title">{{ item.name.replaceAll('-', ' ') }}</h5>
         <p>id: {{ item.id }}</p>
