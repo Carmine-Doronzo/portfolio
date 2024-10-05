f15a4b1bbb17a71c05ffd7e90dd6776743b480fc
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-10-05 21:24:34 +0200

    Add best-icon to best project card

--- src/components/ProjectCardComponent.vue
+++ src/components/ProjectCardComponent.vue
@@ -13,39 +13,20 @@ export default {
 }
 </script>
 
-<!-- <template>
 
-
-
-  <li class="card col-12 col-md-6 col-lg-3 mb-4">
-    <div class="position-relative">
-      <img :src="item.image ? item.image : 'not-avaible.jpg'" class="card-img-top" height="200" alt="...">
-      <img v-if="item.status === 'work in progress'" src="/work-in-progress.jpg" class="position-absolute end-0 top-0"
-        width="100" alt="...">
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">{{ item.name.replaceAll('-', ' ') }}</h5>
-      <p>id: {{ item.id }}</p>
-      <p class="card-text">Status: {{ item.status }}</p>
-      <RouterLink :to="{ name: 'show', params: { slug: item.id } }" class="matrix-button">More ...</RouterLink>
-    </div>
-  </li>
-
-
-
-</template> -->
 <template>
   <li class="card col-12 col-md-6 col-lg-3 mb-4">
     <div class="position-relative">
+      <div class="badge"  v-if="item.status === 'work in progress/best' || item.status === 'finish/best' ">
+        <img src="/best-icon.png" alt="Best Icon" />
+      </div>
       <img :src="item.image ? item.image : 'not-avaible.jpg'" class="card-img-top" height="200" alt="...">
       <WorkInProgressComponent v-if="item.status === 'work in progress' || item.status === 'work in progress/best' " class="position-absolute end-0 top-0 icon-container z-3"/>
-      <!-- <img v-if="item.status === 'work in progress'" src="/work-in-progress.jpg" 
-        class="position-absolute end-0 top-0 icon-container" width="100" alt="...">-->
     </div>
     <div class="card-body">
       <h5 class="card-title">{{ item.name.replaceAll('-', ' ') }}</h5>
       <p>id: {{ item.id }}</p>
-      <p class="card-text">Status: {{ item.status }}</p>
+      <!-- <p class="card-text">Status: {{ item.status }}</p> -->
       <RouterLink :to="{ name: 'show', params: { slug: item.id } }" class="matrix-button">More ...</RouterLink>
     </div>
   </li>
@@ -96,4 +77,17 @@ export default {
   background-color: lime;
   color: black;
 }
+
+.badge {
+  position: absolute;
+  bottom: -122px;
+  right: 10px;
+  background-color: transparent;
+  z-index: 1000;
+}
+.badge img {
+  width: 50px;
+  height: auto;
+}
+
 </style>
